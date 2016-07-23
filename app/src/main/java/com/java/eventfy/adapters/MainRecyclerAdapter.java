921574f86eb627f9b57a0906ab706b5aa99e6f73
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2016-07-23 16:24:36 -0700

    Dependencies updated

--- app/src/main/java/com/java/eventfy/adapters/MainRecyclerAdapter.java
+++ app/src/main/java/com/java/eventfy/adapters/MainRecyclerAdapter.java
@@ -1,12 +1,15 @@
 package com.java.eventfy.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.java.eventfy.Entity.Events;
@@ -21,26 +24,30 @@ import butterknife.ButterKnife;
 /**
  * Created by swapnil on 5/29/16.
  */public class MainRecyclerAdapter extends ArrayRecyclerAdapter<Events, RecyclerView.ViewHolder>{
-
+    public View view;
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new ResultHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.nearby_recycleview, parent, false));
     }
 
+
+
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         switch(holder.getItemViewType()){
             case 0:
                 Events event = getItem(position);
 
-                ((ResultHolder)holder).linearLayout.setOnClickListener(new View.OnClickListener() {
+                Log.e("inside click : ", "");
+                ((ResultHolder)holder).linearLayout.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         // Doesn't do anything, but need Click Listener to get that sweet Ripple
+                        Log.e("in on click listner:  ",""+position);
                     }
                 });
 
-                   LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(Math.abs(DeviceDimensions.deviceWeidth-90),Math.abs(DeviceDimensions.deviceHeight/3));
+                LayoutParams parms = new LayoutParams(Math.abs(DeviceDimensions.deviceWeidth-90),Math.abs(DeviceDimensions.deviceHeight/3));
                  parms.setMargins(45, 16, 0 , 16);
 
                 ((ResultHolder) holder).eventImage.setLayoutParams(parms);
@@ -51,10 +58,6 @@ import butterknife.ButterKnife;
                           .placeholder(R.drawable.img_placeholder)
                           .resize(DeviceDimensions.deviceWeidth-100, Math.abs(DeviceDimensions.deviceHeight/3))
                           .into(((ResultHolder) holder).eventImage);
-
-                Picasso.with(holder.itemView.getContext())
-                        .load(event.getEventImageUrl())
-                        .into(((ResultHolder)holder).eventAdminImage);
 
                 ((ResultHolder)holder).eventName.setText(event.getEventName());
                 ((ResultHolder)holder).eventLocation.setText(event.getEventLocation());
@@ -71,13 +74,12 @@ import butterknife.ButterKnife;
         }
     }
 
+
     public class ResultHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.linear_layout)
         LinearLayout linearLayout;
         @Bind(R.id.event_image)
         ImageView eventImage;
-        @Bind(R.id.event_admin_profile_pic)
-        ImageView eventAdminImage;
         @Bind(R.id.event_name)
         TextView eventName;
         @Bind(R.id.event_location)
@@ -98,5 +100,6 @@ import butterknife.ButterKnife;
         return 0;
 
     }
+
 
 }
