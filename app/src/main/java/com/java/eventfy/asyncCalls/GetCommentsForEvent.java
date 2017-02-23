454295283f02dd3a56e56f915c04f5bc23cd126e
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2017-02-22 17:12:53 -0800

    UI updates about page

--- app/src/main/java/com/java/eventfy/asyncCalls/GetCommentsForEvent.java
+++ app/src/main/java/com/java/eventfy/asyncCalls/GetCommentsForEvent.java
@@ -2,12 +2,22 @@ package com.java.eventfy.asyncCalls;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import com.google.gson.Gson;
 import com.java.eventfy.Entity.Comments;
 import com.java.eventfy.Entity.SignUp;
+import com.java.eventfy.EventBus.EventBusService;
+import com.java.eventfy.R;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -19,6 +29,7 @@ public class GetCommentsForEvent extends AsyncTask<Void, Void, Void> {
     private String url;
     private List<Comments> commentList;
     private SignUp signUp;
+
     private ResponseEntity<Comments[]> response;
     private Context context;
     private  Comments comments = new Comments();
@@ -33,32 +44,32 @@ public class GetCommentsForEvent extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
 
-//        try {
-//
-//
-//            RestTemplate restTemplate = new RestTemplate(true);
-//            restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
-//
-//            HttpEntity<SignUp> request = new HttpEntity<>(signUp);
-//
-//            response = restTemplate.exchange(url, HttpMethod.POST, request, Comments[].class);
-//
-//            Comments[] comments = response.getBody();
-//
-//
-//            commentList = Arrays.asList(comments);
-//
-//            Log.e("Comment list is ", ""+commentList.size());
-//        }catch (Exception e){
-//
-//            commentList = new ArrayList<Comments>();
-//
-//
-//            comments.setViewMessage(context.getString(R.string.home_connection_error));
-//
-//            Log.e("Comment list is excp ", ""+commentList.size());
-//            commentList.add(comments);
-//        }
+        try {
+
+
+            RestTemplate restTemplate = new RestTemplate(true);
+            restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
+
+            HttpEntity<SignUp> request = new HttpEntity<>(signUp);
+
+            response = restTemplate.exchange(url, HttpMethod.POST, request, Comments[].class);
+
+            Comments[] comments = response.getBody();
+
+
+            commentList = Arrays.asList(comments);
+
+            Log.e("Comment list is ", ""+commentList.size());
+        }catch (Exception e){
+
+            commentList = new ArrayList<Comments>();
+
+
+            comments.setViewMessage(context.getString(R.string.home_connection_error));
+
+            Log.e("Comment list is excp ", ""+commentList.size());
+            commentList.add(comments);
+        }
     return null;
     }
 
@@ -66,17 +77,17 @@ public class GetCommentsForEvent extends AsyncTask<Void, Void, Void> {
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
 
-//        if(commentList.size()<=0) {
-//            commentList = new ArrayList<Comments>();
-//            comments.setViewMessage(context.getString(R.string.home_no_data));
-//            commentList.add(comments);
-//        }
-//
-//        for (Comments c : commentList) {
-//            Gson g = new Gson();
-//
-//            Log.e("", "comment is : "+g.toJson(c));
-//        }
-//        EventBusService.getInstance().post(commentList);
+        if(commentList.size()<=0) {
+            commentList = new ArrayList<Comments>();
+            comments.setViewMessage(context.getString(R.string.home_no_data));
+            commentList.add(comments);
+        }
+
+        for (Comments c : commentList) {
+            Gson g = new Gson();
+
+            Log.e("", "comment is : "+g.toJson(c));
+        }
+        EventBusService.getInstance().post(commentList);
     }
 }
